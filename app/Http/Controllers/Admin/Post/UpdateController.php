dec9180f2b6c408414c00a71da963ad8d59f66fb
aleks00s <aeshchepelkin@gmail.com>
2022-09-01 19:13:18 +0300

    Added template for admin panel

---
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Requests\Post\UpdateRequest;
+use App\Models\Post;
+
+
+class UpdateController extends BaseController
+{
+    public function __invoke(UpdateRequest $request, Post $post)
+    {
+        $data = $request->validated();
+        $this->service->update($data, $post);
+        return redirect()->route('posts.show', $post->id);
+    }
+}
